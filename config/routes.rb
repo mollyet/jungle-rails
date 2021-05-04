7ffa6956cf668f5c59222915bccabc199ed15dde
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-04 22:26:11 +0000

    created admin/categories routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ get '/about', to: 'about#index'
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
