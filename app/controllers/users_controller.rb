501284fa28a4bd0884c4a64f616f3802c4ead446
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-05 04:14:20 +0000

    added login/logout/register links to nav bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_ to "/"
+      redirect_to "/"
     else
       redirect_to "/register"
     end
