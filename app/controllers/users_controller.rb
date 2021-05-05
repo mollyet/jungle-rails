c2e591db3e95da848916378e73da5f118b852e0e
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-05 03:58:19 +0000

    added user creation && login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def new
+  end
+  
+  def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -7,9 +10,6 @@ class UsersController < ApplicationController
     else
       redirect_to "/register"
     end
-  end
-  
-  def create
   end
  private
   def user_params
