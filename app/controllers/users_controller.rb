779bc548dd58a2c1c38655db4e5586edc22b2b9f
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-05 03:26:29 +0000

    added user sign up form, added bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_ to "/"
+    else
+      redirect_to "/register"
+    end
+  end
+  
+  def create
+  end
+ private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
