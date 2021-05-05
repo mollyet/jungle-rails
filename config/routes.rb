527a40bef1651ef1b8bb72c127a51fb104c885ac
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-05 03:14:55 +0000

    created users model && contoller

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ get '/about', to: 'about#index'
     resources :categories, only: [:index, :new, :create]
   end
 
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
