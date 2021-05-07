b879f617e5dc62c08656b93b2cef8828c5a61d84
Molly Trepanier <mtrepanier123@gmail.com>
2021-05-07 02:32:24 +0000

    added user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, :password_confirmation, presence: true, length: { minimum: 5 }
-
-
+  def self.authenticate_with_credentials(email, password) 
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password) 
+      return @user
+    else
+      return nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,37 +2,52 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
-        it "works" do
-          #basic test
-          params = {
-            name: "Jimmy",
-            email: "jimmy@dude.com",
-            password: "jimmy",
-            password_confirmation: "jimmy"
-          }
-          @jimmy = User.new(params)
-          expect(@jimmy).to be_valid
-        end
-        it "has a password that is longer than 4 characters" do
-          #dsgdsf
-          params = {
-            name: "Jimmy",
-            email: "jimmy@dude.com",
-            password: "oop",
-            password_confirmation: "oop"
-          }
-          @jimmy = User.new(params)
-          expect(@jimmy).to_not be_valid
-        end
-        it "matches password and password confirmation fields" do
-          params = {
-            name: "Jimmy",
-            email: "jimmy@dude.com",
-            password: "jimmy",
-            password_confirmation: "password"
-          }
-          @jimmy = User.new(params)
-          expect(@jimmy).to_not be_valid
-        end
-      end
+
+    it "works" do
+      #basic test
+      params = {
+        name: "Jimmy",
+        email: "jimmy@dude.com",
+        password: "jimmy",
+        password_confirmation: "jimmy"
+      }
+      # @jimmy = User.new(params)
+      # expect(@jimmy).to be_valid
+      expect(User.new(params)).to be_valid
     end
+    it "has a password that is longer than 4 characters" do
+      params = {
+        name: "Jimmy",
+        email: "jimmy@dude.com",
+        password: "oop",
+        password_confirmation: "oop"
+      }
+      @jimmy = User.new(params)
+      expect(@jimmy).to_not be_valid
+    end
+    it "matches password and password confirmation fields" do
+      params = {
+        name: "Jimmy",
+        email: "jimmy@dude.com",
+        password: "jimmy",
+        password_confirmation: "password"
+      }
+      @jimmy = User.new(params)
+      expect(@jimmy).to_not be_valid
+    end
+  end
+  describe ".authenticate_with_credentials" do
+    it "logs in with authenticated credentials" do
+      params = {
+        name: "Jimmy",
+        email: "jimmy@dude.com",
+        password: "jimmy",
+        password_confirmation: "jimmy"
+      }
+      jimmy = User.new(params)
+      jimmy.save
+      authenticated_user = User.authenticate_with_credentials("jimmy@dude.com", "jimmy")
+      expect(authenticated_user).to eq(jimmy)
+    end
+  end
+end
